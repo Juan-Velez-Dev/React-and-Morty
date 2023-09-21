f44e2e42e017403533f07fab34cd5f6427ff16e3
Juan Velez <juanvelez.dev@gmail.com>
2023-09-21 11:45:59 -0500

    changes in server

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -1,32 +1,34 @@
+// Importamos la librería Axios para realizar solicitudes HTTP.
 const axios = require("axios");
 
+// Definimos una función llamada getCharById que toma un ID y un objeto 'res' como parámetros.
 module.exports = getCharById = (id, res) => {
-  axios(`https://rickandmortyapi.com/api/character/${id}`)
-    .then(({ data }) => {
-      if (data.name) {
-
-        const character = {
-          id: id,
-          name: data.name,
-          gender: data.gender,
-          species: data.species,
-          origin: data.origin,
-          image: data.image,
-          status: data.status,
-          location: data.location
-        }
-        res
-          .writeHead(200, { "content-type": "application/json" })
-          .end(JSON.stringify(character))
-      } else {
-        return;
+  // Realizamos una solicitud GET a la API de Rick and Morty usando Axios.
+  axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(({ data }) => { // Cuando la solicitud se completa con éxito, manejamos la respuesta.
+      // Creamos un objeto 'character' con algunos datos específicos de la respuesta.
+      const character = {
+        id: id,
+        name: data.name,
+        gender: data.gender,
+        species: data.species,
+        origin: data.origin,
+        image: data.image,
+        status: data.status,
+        location: data.location
       }
+
+      // Configuramos la respuesta HTTP con un código 200 y el tipo de contenido JSON.
+      return res
+        .writeHead(200, { "content-type": "application/json" })
+        .end(JSON.stringify(character))
+
     })
-    .catch(err => {
-      console.log(err)
+    .catch(error => { // Si hay un error en la solicitud, manejamos la excepción aquí.
+      // Configuramos la respuesta HTTP con un código 500 y un mensaje de error en formato JSON.
       res
         .writeHead(500, { "content-type": "text/plain" })
-        .end(JSON.stringify({ message: "Not found" }))
+        .end(JSON.stringify(error.message))
     }
     )
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,27 @@
+// Importamos el módulo 'http' para crear un servidor HTTP.
 const http = require("http");
-const getCharById = require("./controllers/getCharById")
 
+// Importamos la función 'getCharById' desde el archivo './controllers/getCharById'.
+const getCharById = require("./controllers/getCharById");
+
+// Definimos el número de puerto en el que escuchará el servidor.
 const PORT = 3001;
 
+// Creamos un servidor HTTP utilizando el módulo 'http'.
 http
   .createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // le da acceso a { "*" } todos
-    //* routes
-    if (req.url.includes("/rickandmorty/character")) {
-      let id = req.url.split("/").pop();
-      getCharById(id, res)
-    }
+    // Configuramos los encabezados de respuesta para permitir el acceso desde cualquier origen.
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Le da acceso a todos los orígenes (cualquier dominio).
 
+    // Verificamos si la URL de la solicitud incluye "/rickandmorty/character".
+    if (req.url.includes("/rickandmorty/character")) {
+      // Extraemos el ID del personaje de la URL.
+      let id = req.url.split("/").pop();
+
+      // Llamamos a la función 'getCharById' para obtener y enviar los detalles del personaje.
+      getCharById(id, res);
+    }
   })
   .listen(PORT, "127.0.0.1", () => {
-    console.log(`Server listening in port ${PORT}`)
-  })
+    console.log(`Server listening on port ${PORT}`);
+  });
